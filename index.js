a43b84e87f668dfafdd6a50c40166a40dcbdcd97
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2021-01-21 18:40:31 +0100

    Avancée et création commandes
    Ajout !serverinfo
    Ajout !userinfo

--- index.js
+++ index.js
@@ -36,6 +36,37 @@ client.on("message", async message =>{
     let messageArray = message.content.split(" ");
     let command = messageArray[0];
     let args = messageArray.slice(1);
-})
+
+    ///Commande User-info
+    if(message.content.startsWith((config.Prefix) +`userinfo`) ){
+
+        let UserEmbed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag)
+        .setThumbnail(message.author.displayAvatarURL({dynamic: true, size: 512}))
+        .addField(`Nom de l'utilisateur`, (message.author.username))
+        .addField(`Tag`, message.author.tag)
+        .addField(`Id`, message.author.id)
+        .addField(`Statut`, message.author.presence.status)
+        .addField(`Compte crée le`, message.author.createdAt)
+        .setFooter('Demandé par ' + message.author.username)
+
+        message.channel.send(UserEmbed)
+    }
+
+    /// Commande ServerInfo
+    if(message.content.startsWith((config.Prefix) +`serverinfo`) ){
+
+        let ServerEmbed = new Discord.MessageEmbed()
+        .setAuthor(message.guild.name)
+        .setThumbnail(message.guild.iconURL({dynamic: true, size: 512}))
+        .addField(`Nom du serveur`, (message.guild.name))
+        .addField(`Propriétaire`, message.guild.owner)
+        .addField(`Nombre de membres`, message.guild.memberCount)
+        .addField(`Nombre de roles`, message.guild.roles.cache.size)
+        .setFooter('Demandé par ' + message.author.username)
+
+        message.channel.send(ServerEmbed)
+    }
+});
 
 client.login(config.Token)
